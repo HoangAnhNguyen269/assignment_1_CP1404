990c46472caeeacb7131b5db24ea6c92d65f71c1
HoangAnhNguyen269 <74226464+HoangAnhNguyen269@users.noreply.github.com>
2021-01-07 13:43:43 +0700

    reformat code for unreliable_car_test.py

--- practical/prac8/unreliable_car_test.py
+++ practical/prac8/unreliable_car_test.py
@@ -3,9 +3,11 @@ CP1404 Practical
 unreliable car test
 """
 from unreliable_car import UnreliableCar
+
+
 def main():
-    good_car = UnreliableCar("good car",100,80)
-    bad_car = UnreliableCar("bad car",100,10)
+    good_car = UnreliableCar("good car", 100, 80)
+    bad_car = UnreliableCar("bad car", 100, 10)
 
     for i in range(10):
         print("try to drive {} km".format(i))
@@ -13,6 +15,7 @@ def main():
         print("{:10} drove {:2}km".format(bad_car.name, bad_car.drive(i)))
     print(good_car)
     print(bad_car)
+
 
 if __name__ == '__main__':
     main()
