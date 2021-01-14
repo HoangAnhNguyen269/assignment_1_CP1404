668d71d6b8741ec63465d6c745a586cecbbaf3ff
HoangAnhNguyen269 <74226464+HoangAnhNguyen269@users.noreply.github.com>
2021-01-14 13:08:02 +0700

    Clean up (remove) any commented-out code or TODOs/comments from the demo that dont need in the cleanup_files.py

--- practical/prac9/cleanup_files.py
+++ practical/prac9/cleanup_files.py
@@ -7,25 +7,19 @@ import os
 
 
 def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
+    """Process all subdirectories using os.walk()."""
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
 
-    # Change to desired directory
-    os.chdir('Lyrics/Christmas')
-
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        os.rename(filename, new_name)
+        # TODO: add a loop to rename the files
+        for filename in filenames:
+            initial_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(initial_name, new_name)
 
 
 def get_fixed_filename(filename):
